fd054e9327f7e864947a185ac30ed36432c29a88
Dinesh <Dinesh@C40LAP-11467.india.rsystems.com>
2024-12-27 18:17:08 +0530

    Add more test cases including delimeter test cases

--- src/calculate-operations.test.js
+++ src/calculate-operations.test.js
@@ -2,11 +2,19 @@ const CalculationOperations = require("./calculate-operations");
 
 describe("Calculation TestCases", () => {
   test("Ädd numbers ", () => {
-    let sum = CalculationOperations.add("1\n2,3\n4,\n5,\n6");
-    expect(sum).toBe(21);
+    let sum = CalculationOperations.add("1,2,3");
+    expect(sum).toBe(6);
   });
   test("If empty string is passed then it should return zero ", () => {
     let sum = CalculationOperations.add();
     expect(sum).toBe(0);
+  });
+  test("Allow add method to handle new line between numbers and return total of numbers", () => {
+    let sum = CalculationOperations.add("1\n2,3\n4,\n5,\n6");
+    expect(sum).toBe(21);
+  });
+  test("Support different delimiter and return sum", () => {
+    let sum = CalculationOperations.add("//;\n1;2");
+    expect(sum).toBe(3);
   });
 });
